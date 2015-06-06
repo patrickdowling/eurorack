11e76a9bfa63a55acd9022f46d894f85829d596d
Patrick Dowling <pld@gurkenkiste.com>
2015-06-06 21:47:42 +0200

    DISP_EN pin was also remapped (still untested)

--- braids/drivers/display.cc
+++ braids/drivers/display.cc
@@ -35,16 +35,19 @@
 namespace braids {
 
 #ifndef OLIMEX_STM32
+#define GPIO_DISP_SER GPIOB
 const uint16_t kPinClk = GPIO_Pin_11;
 const uint16_t kPinData = GPIO_Pin_1;
-#define GPIO_DISP_SER GPIOB
+#define GPIO_DISP_CTRL GPIOB
+const uint16_t kPinEnable = GPIO_Pin_10;
 #else
 // To try and support SPI3 on H405, use GPIOC:10 for clk, GPIOC:12 for data
+#define GPIO_DISP_SER GPIOC
 const uint16_t kPinClk = GPIO_Pin_10;
 const uint16_t kPinData = GPIO_Pin_12;
-#define GPIO_DISP_SER GPIOC
+#define GPIO_DISP_CTRL GPIOB
+const uint16_t kPinEnable = GPIO_Pin_1;
 #endif
-const uint16_t kPinEnable = GPIO_Pin_10;
 
 const uint16_t kCharacterEnablePins[] = {
   GPIO_Pin_5,
@@ -63,14 +66,14 @@ void Display::Init() {
   
   gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
   gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOB, &gpio_init);
+  GPIO_Init(GPIO_DISP_CTRL, &gpio_init);
 
   gpio_init.GPIO_Pin = kPinClk | kPinData;
   gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
   gpio_init.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIO_DISP_SER, &gpio_init);
 
-  GPIOB->BSRR = kPinEnable;
+  GPIO_DISP_CTRL->BSRR = kPinEnable;
   active_position_ = 0;
   brightness_pwm_cycle_ = 0;
   brightness_ = 3;
@@ -79,13 +82,13 @@ void Display::Init() {
 
 void Display::Refresh() {
   if (brightness_pwm_cycle_ <= brightness_) {
-    GPIOB->BRR = kCharacterEnablePins[active_position_];
+    GPIO_DISP_CTRL->BRR = kCharacterEnablePins[active_position_];
     active_position_ = (active_position_ + 1) % kDisplayWidth;
     Shift14SegmentsWord(chr_characters[
         static_cast<uint8_t>(buffer_[active_position_])]);
-    GPIOB->BSRR = kCharacterEnablePins[active_position_];
+    GPIO_DISP_CTRL->BSRR = kCharacterEnablePins[active_position_];
   } else {
-    GPIOB->BRR = kCharacterEnablePins[active_position_];
+    GPIO_DISP_CTRL->BRR = kCharacterEnablePins[active_position_];
   }
   brightness_pwm_cycle_ = (brightness_pwm_cycle_ + 1) % kBrightnessLevels;
 }
@@ -95,7 +98,7 @@ void Display::Print(const char* s) {
 }
 
 void Display::Shift14SegmentsWord(uint16_t data) {
-  GPIOB->BRR = kPinEnable;
+  GPIO_DISP_CTRL->BRR = kPinEnable;
   for (uint16_t i = 0; i < 16; ++i) {
     GPIO_DISP_SER->BRR = kPinClk;
     if (data & 1) {
@@ -106,7 +109,7 @@ void Display::Shift14SegmentsWord(uint16_t data) {
     data >>= 1;
     GPIO_DISP_SER->BSRR = kPinClk;
   }
-  GPIOB->BSRR = kPinEnable;
+  GPIO_DISP_CTRL->BSRR = kPinEnable;
 }
 
 }  // namespace braids
